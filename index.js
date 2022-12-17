8139dbf2f304eeeaa1e76bc4caaab5ca054490d6
Jurasia <jurasiabritton@gmail.com>
2022-12-17 14:41:45 -0500

    commit tree c4dfb3a07c0e44bcf43d359ba2ddbedd3a1ab80b

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+
+ function upperCaseCustomerName() {
+
+     customerName = "BOB";
+    
+ }
+
+
+function setBestCustomer () {
+ bestCustomer = "not bob";
+     
+ }
+ 
+
+function overwriteBestCustomer () {
+    bestCustomer = "maybe bob";
+}   
+
+function changesLeastFavoriteCustomer() {
+     leastFavoriteCuster = "mike";
+}
+const leastFavoriteCustomer = "bob";
