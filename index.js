095d2aea93845a8f29166b0e5a8aef99645e722a
Jurasia <jurasiabritton@gmail.com>
2023-01-10 09:40:18 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+const leastFavoriteCustomer = "mike";
+
+ function upperCaseCustomerName() {
+
+     customerName = "BOB";
+    
+ }
+
+
+function setBestCustomer () {
+ bestCustomer = "not bob";
+     
+ }
+ 
+
+function overwriteBestCustomer () {
+    bestCustomer = "maybe bob";
+}   
+
+function changeLeastFavoriteCustomer() {
+     leastFavoriteCustomer = "bob";
+}
+
+
